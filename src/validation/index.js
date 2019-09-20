82be658dee436da4357ed80b5cf5788a3e7aba8e
Igor Vasiliev <helmsman@gmail.com>
2019-09-20 10:33:06 +0300

    Error message translations

--- src/validation/index.js
+++ src/validation/index.js
@@ -1,9 +1,11 @@
+import i18n from "../i18n";
+
 export const requiredInput = (input) => {
-  return (input) ? undefined : 'Требуется ввод';
+  return (input) ? undefined : i18n.t('errRequiredInput');
 }
 
 export const correctInput = (input) => {
   // eslint-disable-next-line
   var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  return (re.test(String(input).toLowerCase()) === false) ? 'Неправильный формат email' : undefined;
+  return (re.test(String(input).toLowerCase()) === false) ? i18n.t('errWrongEmailFormat') : undefined;
 }
